34d6a05a69a5e3779f2503017ca2917b40f80142
Gordon <gordonzhou68@outlook.com>
2019-10-02 21:28:59 -0400

    component generated dynamically

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Information needed to display on cards
 const data = [
     {
         id: 1,
@@ -21,50 +22,57 @@ const data = [
         image_source:'./assets/pink-flower.png'
     }
 ];
-const blueFlower = data[0];
 
-//Elements needed to construct HTML component
-const body = document.querySelector('body');
-const card_container = document.createElement('div');
-const card = document.createElement('div');
-const image = document.createElement('img');
-const card_body = document.createElement('div');
-const h4 = document.createElement('h4');
-const price = document.createElement('p');
-const rating = document.createElement('p');
-const star = document.createElement('img');
-//insert information for stars needs to be placed before the loop
-star.setAttribute('src', './assets/star.svg');
-star.setAttribute('style', 'width: 8px');
-star.setAttribute('alt', 'star');
+const buildProductCard = (product) => {
+
+    //Create elements to build HTML card component
+    const card_container = document.querySelector('.card-container');
+    const card = document.createElement('div');
+    const image = document.createElement('img');
+    const card_body = document.createElement('div');
+    const h4 = document.createElement('h4');
+    const price = document.createElement('p');
+    const rating = document.createElement('p');
+    const star = document.createElement('img');
+    //svg attributes need to be set before the loop of appending it begins
+    star.setAttribute('src', './assets/star.svg');
+    // star.setAttribute('style', 'width: 8px; opacity: 0.5');
+    star.setAttribute('alt', 'star');
+
+    // Append created elements into the DOM
+    for (let i = 0; i < product.rating; i++) {
+        star.setAttribute('style', 'width: 8px');
+        rating.appendChild(star.cloneNode(true))
+    }
+
+    for (let i = 0; i < (5 - product.rating); i++) {
+        star.setAttribute('style', 'width: 8px; opacity: 0.5');
+        rating.appendChild(star.cloneNode(true))
+    }
+    card_container.append(card);
+    card.append(image);
+    card.append(card_body);
+    card_body.append(h4);
+    card_body.append(price);
+    card_body.append(rating);
 
 
-for (let i = 0; i < blueFlower.rating; i++) {
-    rating.appendChild(star.cloneNode(true))
+    //Set content and attributes
+    rating.setAttribute('class', 'rating');
+    card.setAttribute('class', 'card');
+    image.setAttribute('class', 'image');
+    image.setAttribute('src', product.image_source);
+    image.setAttribute('alt', product.product_name);
+    card_body.setAttribute('class', 'card-body');
+    price.setAttribute('class', 'price');
+    price.innerHTML = product.price;
+    h4.innerHTML = product.product_name;
 }
-body.append(card_container);
-card_container.append(card);
-card.append(image);
-card.append(card_body);
-card_body.append(h4);
-card_body.append(price);
-card_body.append(rating);
 
 
-//Add information
-rating.setAttribute('class', 'rating');
-
-card_container.setAttribute('class', 'card_container');
-card.setAttribute('class', 'card');
-
-image.setAttribute('class', 'image');
-image.setAttribute('src', './assets/blue-flower.png');
-image.setAttribute('alt', 'blue-flower');
-
-card_body.setAttribute('class', 'card_body');
-price.setAttribute('class', 'price');
-price.innerHTML = blueFlower.price;
-h4.innerHTML = blueFlower.product_name;
+data.forEach(function(product){
+    buildProductCard(product)
+})
 
 
 
