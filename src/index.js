75bd9d2bc2174af0f212106fa2b9966d1f9f6969
Gordon <gordonzhou68@outlook.com>
2019-10-04 22:35:46 -0400

    add button and badge to the script so that every html element gets created dynamically

--- src/index.js
+++ src/index.js
@@ -28,7 +28,10 @@ const buildProductCard = (product) => {
     //Create elements to build HTML card component
     const card_container = document.querySelector('.card-container');
     const card = document.createElement('div');
+    const imageGroup = document.createElement('div');
     const image = document.createElement('img');
+    const button = document.createElement('button');
+    const badge = document.createElement('div');
     const card_body = document.createElement('div');
     const h4 = document.createElement('h4');
     const price = document.createElement('p');
@@ -36,7 +39,6 @@ const buildProductCard = (product) => {
     const star = document.createElement('img');
     //svg attributes need to be set before the loop of appending it begins
     star.setAttribute('src', './assets/star.svg');
-    // star.setAttribute('style', 'width: 8px; opacity: 0.5');
     star.setAttribute('alt', 'star');
 
     // Append created elements into the DOM
@@ -50,7 +52,10 @@ const buildProductCard = (product) => {
         rating.appendChild(star.cloneNode(true))
     }
     card_container.append(card);
-    card.append(image);
+    imageGroup.append(image);
+    imageGroup.append(button);
+    imageGroup.append(badge);
+    card.append(imageGroup);
     card.append(card_body);
     card_body.append(h4);
     card_body.append(price);
@@ -60,9 +65,14 @@ const buildProductCard = (product) => {
     //Set content and attributes
     rating.setAttribute('class', 'rating');
     card.setAttribute('class', 'card');
+    imageGroup.setAttribute('class', 'imageGroup');
     image.setAttribute('class', 'image');
     image.setAttribute('src', product.image_source);
     image.setAttribute('alt', product.product_name);
+    button.setAttribute('type', 'button');
+    button.innerHTML = 'Add to Cart';
+    badge.setAttribute('class', 'badge');
+    badge.innerHTML = 'In Cart';
     card_body.setAttribute('class', 'card-body');
     price.setAttribute('class', 'price');
     price.innerHTML = product.price;
